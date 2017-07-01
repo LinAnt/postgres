c473b8b9bc460d9fc95de64b847f2973fb60659e
tamal <tamal@appscode.com>
2017-07-01 10:54:42 -0700

    Examples moved to k8sdb/cli/docs

--- example/statefulset/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl delete sa governing-postgres
-kubectl delete service postgres-demo
-kubectl delete secret postgres-demo-admin-auth
-kubectl delete statefulset kubedb-postgres-demo
--- example/statefulset/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-REPO_ROOT=$GOPATH/src/github.com/k8sdb/postgres
-example=$REPO_ROOT/example/statefulset
-
-kubectl create -f $example/postgres.yaml
-kubectl create -f $example/service.yaml
-kubectl create -f $example/secret.yaml
-kubectl create -f $example/service-account.yaml
