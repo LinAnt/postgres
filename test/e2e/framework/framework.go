1f1a9eb34e6ef2fd5e80bd490684b3a46366f702
tamal <tamal@appscode.com>
2017-10-22 06:25:12 -0700

    Change k8s.io/client-go/kubernetes alias to kubernetes

--- test/e2e/framework/framework.go
+++ test/e2e/framework/framework.go
@@ -3,18 +3,18 @@ package framework
 import (
 	"github.com/appscode/go/crypto/rand"
 	tcs "github.com/k8sdb/apimachinery/client/typed/kubedb/v1alpha1"
-	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes"
 )
 
 type Framework struct {
-	kubeClient   clientset.Interface
+	kubeClient   kubernetes.Interface
 	extClient    tcs.KubedbV1alpha1Interface
 	namespace    string
 	name         string
 	StorageClass string
 }
 
-func New(kubeClient clientset.Interface, extClient tcs.KubedbV1alpha1Interface, storageClass string) *Framework {
+func New(kubeClient kubernetes.Interface, extClient tcs.KubedbV1alpha1Interface, storageClass string) *Framework {
 	return &Framework{
 		kubeClient:   kubeClient,
 		extClient:    extClient,
