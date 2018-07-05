8933c3c621fe4ca69b59411f38b146eceb5a01c9
Anton Lindholm <anton.c.e.lindholm@gmail.com>
2018-07-05 12:50:54 +0300

    Fix to use precompiled regexp in favor of string.

--- pkg/admission/validator.go
+++ pkg/admission/validator.go
@@ -122,7 +122,7 @@ func (a *PostgresValidator) Admit(req *admission.AdmissionRequest) *admission.Ad
 
 var (
 	postgresVersions      = sets.NewString("9.6", "9.6.7", "10.2")
-	postgresCustomVersion = `(\d+\.){1,2}\d+-` // Matches x.x.x-<own-version> x.x-<own-version>
+	postgresCustomVersion = regexp.MustCompile(`(\d+\.){1,2}\d+-`) // Matches x.x.x-<own-version> x.x-<own-version>
 )
 
 // ValidatePostgres checks if the object satisfies all the requirements.
@@ -134,10 +134,7 @@ func ValidatePostgres(client kubernetes.Interface, extClient kubedbv1alpha1.Kube
 
 	// Check Postgres version validation
 	if !postgresVersions.Has(string(postgres.Spec.Version)) {
-		ok, err := regexp.MatchString(postgresCustomVersion, string(postgres.Spec.Version))
-		if err != nil {
-			return fmt.Errorf(`KubeDB doesn't support this custom Postgres version: %s, Error: %s`, string(postgres.Spec.Version), err)
-		}
+		ok := postgresCustomVersion.MatchString(string(postgres.Spec.Version))
 		if !ok {
 			return fmt.Errorf(`KubeDB doesn't support Postgres version: %s`, string(postgres.Spec.Version))
 		}
